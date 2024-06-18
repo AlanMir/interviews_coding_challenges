b9a909888cd9ea8983ea561eb7aef4a2f167b41c
AlanMir <49082494+AlanMir@users.noreply.github.com>
2024-06-17 22:46:37 -0700

    add query to identify the latest person in the bus
    this query identifies the last person that can fit in a bus with a weight limit

---
+++ Last Person to Fit in the Bus.sql
@@ -0,0 +1,36 @@
+/*
+Table: Queue
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| person_id   | int     |
+| person_name | varchar |
+| weight      | int     |
+| turn        | int     |
++-------------+---------+
+person_id column contains unique values.
+This table has the information about all people waiting for a bus.
+The person_id and turn columns will contain all numbers from 1 to n, where n is the number of rows in the table.
+turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
+weight is the weight of the person in kilograms.
+ 
+
+There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
+
+Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
+
+
+*/
+with capacity_calculations as (
+select person_name
+,1000-sum(weight) over (order by turn) as remaining_capacity
+from queue
+order by turn
+)
+select person_name
+from capacity_calculations
+where remaining_capacity >= 0
+order by remaining_capacity
+limit 1
+;
